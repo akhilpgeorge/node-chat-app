7eaf941351d611aab80fea458ebeac2e96d07541
akhilpgeorge@gmail.com <akhilpgeorge@gmail.com>
2017-12-13 15:34:15 +0530

    add new message and create new message events

--- server/server.js
+++ server/server.js
@@ -19,6 +19,18 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('new user connected');
+
+    socket.emit('newMessage', {
+        from: 'akhil',
+        text: 'hey hw r u??',
+        createdAt: 123
+    });
+
+
+    socket.on('createMessage',(message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('user diconnected');
     });
